8b59dac8b03d68e31210ec2200c70eaf9c367ade
Renan Iglesias <rengenesio@gmail.com>
2021-05-16 17:13:46 -0300

    [@rengenesio] Add class that decapsulates log records.

---
+++ src/QuakeLogSummarizer.Core/LogMessageExtractor.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+namespace QuakeLogSummarizer.Core
+{
+    /// <summary>
+    /// Logged messages are joined with a time prefix to be recorded to the log file.
+    /// This prefix format specifier is: '%3i:%i%i '. This class removes the time prefix from a log record.
+    /// </summary>
+    /// <remarks>
+    /// Reference: 'G_LogPrintf' function at https://github.com/id-Software/Quake-III-Arena/blob/master/code/game/g_main.c
+    /// </remarks>
+    public sealed class LogMessageExtractor
+    {
+        // Extracts a message from the record using the following regex:
+        //     - '^(?=.{3}:)' - positive lookahead to ensure the input starts with 3 characters before the first colon
+        //     - '( *\d+:)' - the first 3 characters should be zero or more whitespaces followed by at least one digit (the minutes portion of time)
+        //     - '\d{2} ' - two digits and a whitespace after the first colon (the seconds portion of time and a separator after the logged time)
+        //     - '([(\[a-zA-Z\]+:)(\-+)].*)' - captures everything after the time separator (should be only letters (at least one) followed by a colon; or a hyphen sequence)
+        // NOTE: This regex was based on all arguments used on 'G_LogPrintf' functions (from Quake III source code 'https://github.com/id-Software/Quake-III-Arena')
+        private readonly Regex _extractMessageDataRegex = new Regex(@"^(?=.{3}:)( *\d+:)\d{2} ([(\[a-zA-Z\]+:)(\-+)].*)");
+
+        /// <returns>
+        /// The logged message data without the time prefix or 'null' if <paramref name="logRecord"/> doesn't match the expected format.
+        /// </returns>
+        public string Extract(string logRecord)
+        {
+            Match match = this._extractMessageDataRegex.Match(logRecord);
+            if (match?.Success == false)
+            {
+                return null;
+            }
+
+            return match.Groups[2].ToString();
+        }
+    }
+}
---
+++ test/QuakeLogSummarizer.UnitTest/Core/LogMessageExtractorTest.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using QuakeLogSummarizer.Core;
+using Xunit;
+
+namespace QuakeLogSummarizer.UnitTest
+{
+    public sealed class LogMessageExtractorTest
+    {
+        private readonly LogMessageExtractor _extractor;
+
+        public LogMessageExtractorTest()
+        {
+            this._extractor = new LogMessageExtractor();
+        }
+
+        [Theory]
+        // Some possible values that meets the log format:
+        [InlineData(@"  1:23 Record with message",
+                    @"Record with message")]
+        // Real log examples:
+        [InlineData(@"  0:00 ------------------------------------------------------------",
+                    "------------------------------------------------------------")]
+        [InlineData(@"  0:00 InitGame: \sv_floodProtect\1\sv_maxPing\0\sv_minPing\0\sv_maxRate\10000\sv_minRate\0\sv_hostname\Code Miner Server\g_gametype\0\sv_privateClients\2\sv_maxclients\16\sv_allowDownload\0\dmflags\0\fraglimit\20\timelimit\15\g_maxGameClients\0\capturelimit\8\version\ioq3 1.36 linux - x86_64 Apr 12 2009\protocol\68\mapname\q3dm17\gamename\baseq3\g_needpass\0",
+                    @"InitGame: \sv_floodProtect\1\sv_maxPing\0\sv_minPing\0\sv_maxRate\10000\sv_minRate\0\sv_hostname\Code Miner Server\g_gametype\0\sv_privateClients\2\sv_maxclients\16\sv_allowDownload\0\dmflags\0\fraglimit\20\timelimit\15\g_maxGameClients\0\capturelimit\8\version\ioq3 1.36 linux - x86_64 Apr 12 2009\protocol\68\mapname\q3dm17\gamename\baseq3\g_needpass\0")]
+        [InlineData(@" 20:37 ClientBegin: 2",
+                    @"ClientBegin: 2")]
+        [InlineData(@"981:06 ClientConnect: 2",
+                    @"ClientConnect: 2")]
+        private void Extract_When_LogRecordHasValidFormat_Should_ReturnExpectedLogMessage(string logRecord, string expected)
+        {
+            // Act
+            string actual = this._extractor.Extract(logRecord);
+
+            // Assert
+            actual.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("Record: Without any prefix")]
+        [InlineData("   Record: With 3 leading whitespaces without time")]
+        [InlineData("1 2:34 Record: With invalid minutes portion in time")]
+        [InlineData("1:23 Record: With less leading whitespaces than expected")]
+        [InlineData("12:34 Record:With less leading whitespaces than expected")]
+        [InlineData("   1:23 Record: With more leading whitespaces than expected")]
+        [InlineData("  12:34 Record: With more leading whitespaces than expected")]
+        [InlineData(" 123:45 Record: With more leading whitespaces than expected")]
+        [InlineData("123:456 Record: With more digits than expected")]
+        [InlineData("123:45Record: Without whitespace separetor before log message")]
+        [InlineData("123:45_Record: Without whitespace separetor before log message")]
+        [InlineData(" 12:34 56:78 Record: log at '12:34' was truncated and another record was logged after it")]
+        [InlineData(" 12:34 ")] // Record truncated and without other record logged after it
+        private void Extract_When_LogRecordHasInvalidFormat_Should_ReturnNull(string logRecord)
+        {
+            // Act
+            string actual = this._extractor.Extract(logRecord);
+
+            // Assert
+            actual.Should().BeNull();
+        }
+    }
+}
