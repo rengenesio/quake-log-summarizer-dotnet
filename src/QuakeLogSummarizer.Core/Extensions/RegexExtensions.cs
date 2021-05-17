55a2ee8488bfaaea0b01082946fa71ab65d0b81a
Renan Iglesias <rengenesio@gmail.com>
2021-05-17 00:09:21 -0300

    [@rengenesio] Add extension method to generate regex from format strings.

---
+++ src/QuakeLogSummarizer.Core/Extensions/RegexExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace QuakeLogSummarizer.Core.Extensions
+{
+    public static class RegexExtensions
+    {
+        /// <summary>
+        /// Contains pairs of a format specifier and an equivalent pattern matching regex.
+        /// </summary>
+        public static readonly IReadOnlyList<(string FormatSpecifier, string RegexPattern)> FormatReplacementList = new List<(string, string)>()
+        {
+            // Be aware of using non-greedy regex.
+            ("%i", @"(-?\d+?)"),
+            ("%s", @"(.*?)")
+        };
+
+        /// <summary>
+        /// Converts a string containing format specifiers to an equivalent pattern matching regex.
+        /// Useful to generate regex to extract data from a formatted string.
+        /// Refer to <see cref="FormatReplacementList"/> to get a list of supported format specifiers.
+        /// </summary>
+        /// <param name="format">String containing format specifiers to be converted.</param>
+        /// <returns>Equivalent pattern matching regex.</returns>
+        /// <remarks>Input format reference: 'https://www.cplusplus.com/reference/cstdio/printf/'</remarks>
+        public static Regex ToRegex(this string format)
+        {
+            foreach((string formatSpecifier, string regexPattern) in RegexExtensions.FormatReplacementList)
+            {
+                format = format.Replace(formatSpecifier, regexPattern);
+            }
+
+            return new Regex($"^{format}$", RegexOptions.Compiled);
+        }
+    }
+}
---
+++ test/QuakeLogSummarizer.UnitTest/Core/Extensions/RegexExtensionsTest.cs
@@ -0,0 +1,76 @@
+﻿using System.Text.RegularExpressions;
+using AutoFixture;
+using FluentAssertions;
+using QuakeLogSummarizer.Core.Extensions;
+using Xunit;
+
+namespace QuakeLogSummarizer.UnitTest.Extensions
+{
+    public sealed class RegexExtensionsTest
+    {
+        private readonly Fixture _fixture = new Fixture();
+
+        [Fact]
+        private void ToRegex_When_FormatStringNotContainsFormatIdentifier_Should_ReturnRegexWithoutCapture()
+        {
+            // Arrange
+            string formatString = "Test string without any format specifier.";
+
+            // Act
+            Regex actual = formatString.ToRegex();
+
+            // Assert
+            actual.IsMatch(formatString).Should().BeTrue();
+        }
+
+        [Fact]
+        private void ToRegex_When_FormatStringContainsIntegerFormatSpecifier_Should_ExtractIntegerValue()
+        {
+            // Arrange
+            int expectedInteger = this._fixture.Create<int>();
+            string formatString = "Test string containing the integer %i as formatted value.";
+            string formattedString = $"Test string containing the integer {expectedInteger} as formatted value.";
+
+            // Act
+            Regex actual = formatString.ToRegex();
+
+            // Assert
+            actual.Match(formattedString).Groups[1].ToString()
+                .Should().Be(expectedInteger.ToString());
+        }
+
+        [Fact]
+        private void ToRegex_When_FormatStringContainsStringFormatSpecifier_Should_ExtractStringValue()
+        {
+            // Arrange
+            string expectedString = this._fixture.Create<string>();
+            string formatString = "Test string containing the string %s as formatted value.";
+            string formattedString = $"Test string containing the string {expectedString} as formatted value.";
+
+            // Act
+            Regex actual = formatString.ToRegex();
+
+            // Assert
+            actual.Match(formattedString).Groups[1].ToString()
+                .Should().Be(expectedString);
+        }
+
+        [Fact]
+        private void ToRegex_When_FormatStringContainsStringAndIntFormatSpecifiers_Should_ExtractBothValues()
+        {
+            // Arrange
+            int expectedInteger = this._fixture.Create<int>();
+            string expectedString = this._fixture.Create<string>();
+            string formatString = "Test string containing %i %s as formatted values.";
+            string formattedString = $"Test string containing {expectedInteger} {expectedString} as formatted values.";
+
+            // Act
+            Regex actual = formatString.ToRegex();
+
+            // Assert
+            Match match = actual.Match(formattedString);
+            match.Groups[1].ToString().Should().Be(expectedInteger.ToString());
+            match.Groups[2].ToString().Should().Be(expectedString);
+        }
+    }
+}
