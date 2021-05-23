74f9a64e185dcee56c3c290cdf2b53ddf6fa539e
Renan Iglesias <rengenesio@gmail.com>
2021-05-23 19:40:13 -0300

    [@rengenesio] Add class to serialize specific JSON.

---
+++ src/QuakeLogSummarizer.Core/Converters/KeyValuePairListConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace QuakeLogSummarizer.Core.Converters
+{
+    /// <summary>
+    /// Converts a list containing key/value pairs to JSON as a dictionary.
+    /// Useful when needs serialize a dictionary containing duplicated keys.
+    /// </summary>
+    /// <remarks>
+    /// Only the write function is implemented currently (serialization). It doesn't support deserialize a JSON string into an object.
+    /// </remarks>
+    public class KeyValuePairListConverter : JsonConverter<IEnumerable<(string PropertyName, int Value)>>
+    {
+        /// <inheritdoc />
+        public override IEnumerable<(string, int)> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public override void Write(Utf8JsonWriter writer, IEnumerable<(string PropertyName, int Value)> value, JsonSerializerOptions options)
+        {
+            writer.WriteStartObject();
+            foreach ((string PropertyName, int Value) item in value)
+            {
+                writer.WritePropertyName(item.PropertyName);
+                writer.WriteNumberValue(item.Value);
+            }
+
+            writer.WriteEndObject();
+        }
+    }
+}
---
+++ test/QuakeLogSummarizer.UnitTest/Core/Converters/KeyValuePairListConverterTest.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using AutoFixture;
+using FluentAssertions;
+using QuakeLogSummarizer.Core.Converters;
+using Xunit;
+
+namespace QuakeLogSummarizer.UnitTest.Core.Converters
+{
+    public sealed class KeyValuePairListConverterTest
+    {
+        private readonly JsonSerializerOptions _jsonSerializerOptions;
+        private readonly Fixture _fixture;
+
+        public KeyValuePairListConverterTest()
+        {
+            this._fixture = new Fixture();
+
+            this._jsonSerializerOptions = new JsonSerializerOptions();
+            this._jsonSerializerOptions.WriteIndented = false;
+            this._jsonSerializerOptions.Converters.Add(new KeyValuePairListConverter());
+
+        }
+
+        [Fact]
+        private void Serialize_When_ListIsEmpty_Should_SerializeEmptyDictionaryJson()
+        {
+            // Arrange
+            IEnumerable<(string, int)> keyValuePairList = new List<(string, int)>();
+
+            // Act
+            string actual = JsonSerializer.Serialize(keyValuePairList, this._jsonSerializerOptions);
+
+            // Assert
+            IDictionary<string, int> actualDictionary = JsonSerializer.Deserialize<IDictionary<string, int>>(actual);
+            actualDictionary.Should().BeEmpty();
+        }
+
+        [Fact]
+        private void Serialize_When_ListContainsElements_Should_SerializeAsDictionaryJson()
+        {
+            // Arrange
+            IEnumerable<(string Key, int Value)> keyValuePairList = this._fixture.CreateMany<(string, int)>();
+
+            // Act
+            string actual = JsonSerializer.Serialize(keyValuePairList, this._jsonSerializerOptions);
+
+            // Assert
+            IDictionary<string, int> actualDictionary = JsonSerializer.Deserialize<IDictionary<string, int>>(actual);
+            IEnumerable<KeyValuePair<string, int>> expectedKeyValuePairList = keyValuePairList.Select(p => new KeyValuePair<string, int>(p.Key, p.Value));
+
+            actualDictionary.Should().Contain(expectedKeyValuePairList);
+        }
+    }
+}
